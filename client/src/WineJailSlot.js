76bbea49ae8c078203269f2418c527938defe635
Matt Higgins <matthew.alan.higgins@gmail.com>
2018-10-20 08:28:36 -0700

    Adds popover for each wine in jail

--- client/src/WineJailSlot.js
+++ client/src/WineJailSlot.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import { Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 
 class WineJailSlot extends Component {
+  constructor() {
+    super();
+
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+
+    this.state = { popoverOpen: false };
+  }
+
   render() {
     if (this.props.wine) {
-        return <div className="wine-jail-slot full" data-wine={JSON.stringify(this.props.wine)}></div>;
-      } else {
-        return <div className="wine-jail-slot empty"></div>
-      }
+      const wine = JSON.stringify(this.props.wine);
+      const id = `wine-${this.props.wine.id}`;
+      return <div className="wine-jail-slot full"
+                  data-wine={wine}
+                  id={id}
+                  onMouseEnter={this.handleMouseEnter}
+                  onMouseLeave={this.handleMouseLeave}>
+                  <Popover target={id} isOpen={this.state.popoverOpen} placement="auto">
+                    <PopoverHeader>This wine is...</PopoverHeader>
+                    <PopoverBody>{this.getWineDisplayText(this.props.wine)}</PopoverBody>
+                  </Popover>
+              </div>;
+    } else {
+      return <div className="wine-jail-slot empty"></div>
+    }
+  }
+
+  handleMouseEnter() {
+    this.setState({ popoverOpen: true });
+  }
+
+  handleMouseLeave() {
+    this.setState({ popoverOpen: false });
+  }
+
+  getWineDisplayText(wine) {
+    return `${wine.vintage} ${wine.winemaker} ${wine.variety} from ${wine.region}`;
   }
 }
 
--- client/src/spec/WineJailSlot.spec.js
+++ client/src/spec/WineJailSlot.spec.js
@@ -4,26 +4,77 @@ import { shallow } from 'enzyme'
 
 describe("WineJailSlot", () => {
   let component;
+  const wineData = {
+    "id":1,
+    "position": "1",
+    "winemaker": "Cyril Gautheron",
+    "vintage": "2012",
+    "variety": "Chablis",
+    "region": "Burgundy",
+  };
 
   beforeEach(() => {
     component = shallow(<WineJailSlot wine={wineData}/>);
   });
 
-  it('marks a slot as "full" if passed a wine', () => {
-    expect(component.props()['className'].includes("full")).toBe(true);
+  it('initially has state.popoverOpen set to false', () => {
+    expect(component.state("popoverOpen")).toBe(false);
   });
 
-  it('adds the JSON data of the wine as a data attribute if a wine matches its position', () => {
-    const firstSlot = component.find("div[className*='wine-jail-slot']").first();
-    expect(component.props()['data-wine']).toBe(JSON.stringify(wineData));
+  describe("given a wine", () => {
+    beforeEach(() => {
+      component = shallow(<WineJailSlot wine={wineData}/>);
+    });
+  
+    it('marks a slot as "full"', () => {
+      expect(component.props()['className'].includes("full")).toBe(true);
+    });
+
+    it("bases the element's id on the id of the wine", () =>{
+      expect(component.props()['id']).toBe(`wine-${wineData.id}`);
+    });
+  
+    it('adds the JSON data of the wine as a data attribute', () => {
+      expect(component.props()['data-wine']).toBe(JSON.stringify(wineData));
+    });
+
+    it('renders a Popover', () => {
+      const popover = component.find("Popover");
+      expect(popover.length).toBe(1);
+      expect(popover.prop("isOpen")).toBe(false);
+      expect(popover.prop("target")).toBe(`wine-${wineData.id}`);
+      expect(popover.find("PopoverHeader").prop("children")).toBe("This wine is...");
+      expect(popover.find("PopoverBody").prop("children"))
+        .toBe(`${wineData.vintage} ${wineData.winemaker} ${wineData.variety} from ${wineData.region}`);
+    });
+
+    it('opens the popover when a user hovers over the slot', () => {
+      component.simulate("mouseEnter");
+      expect(component.state("popoverOpen")).toBe(true);
+      expect(component.find("Popover").prop("isOpen")).toBe(true);
+    });
+
+    it('closes the popover when a user stops hovering over the slot', () => {
+      component.setState({popoverOpen: true});
+
+      component.simulate("mouseLeave");
+      expect(component.state("popoverOpen")).toBe(false);
+      expect(component.find("Popover").prop("isOpen")).toBe(false);
+    });
   });
 
-  it('marks a slot as "empty" if not passed a wine', () => {
-    const emptySlot = shallow(<WineJailSlot wine={undefined}/>);
-    expect(emptySlot.props()['className'].includes("empty")).toBe(true);
+  describe("given no wine", () => {
+    beforeEach(() => {
+      component = shallow(<WineJailSlot wine={undefined}/>);
+    });
+  
+    it('marks a slot as "empty"', () => {
+      expect(component.props()['className'].includes("empty")).toBe(true);
+    });
+
+    it('does not change state.popoverOpen to true when a user hovers over the slot', () => {
+      component.simulate("mouseEnter");
+      expect(component.state("popoverOpen")).toBe(false);
+    });
   });
 });
-
-const wineData = {
-  "id":1, "position": "1", "winemaker":"Cyril Gautheron","vintage":"2012","variety":"Chablis","region":"Burgundy","created_at":"2018-10-18T23:13:27.842Z","updated_at":"2018-10-18T23:13:27.842Z"
-};
